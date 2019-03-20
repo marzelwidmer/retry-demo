7af949568bda8f6c7acdc5df26a6c276ad8d4c82
Marcel Widmer <marzelwidmer@gmail.com>
2019-03-20 16:51:22 +0100

    change retry exception check

--- src/main/kotlin/ch/keepcalm/example/retrydemo/RetryDemoApplication.kt
+++ src/main/kotlin/ch/keepcalm/example/retrydemo/RetryDemoApplication.kt
@@ -17,13 +17,33 @@ import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
 import org.springframework.web.client.getForEntity
 import javax.xml.ws.ServiceMode
+import org.springframework.retry.backoff.ExponentialBackOffPolicy
+import org.springframework.retry.backoff.BackOffPolicy
+import org.springframework.cloud.client.loadbalancer.LoadBalancedRetryFactory
+import org.springframework.remoting.RemoteAccessException
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.EnableRetry
+import org.springframework.retry.annotation.Recover
+import org.springframework.web.client.HttpClientErrorException
+
 
 @SpringBootApplication
-@Retryable
+@EnableRetry
 class RetryDemoApplication() {
     @Bean
     fun restTemplate(restTemplateBuilder: RestTemplateBuilder): RestTemplate {
         return restTemplateBuilder.build()
+    }
+
+
+
+    @Bean
+    fun retryFactory(): LoadBalancedRetryFactory {
+        return object : LoadBalancedRetryFactory {
+            override fun createBackOffPolicy(service: String?): BackOffPolicy {
+                return ExponentialBackOffPolicy()
+            }
+        }
     }
 }
 
@@ -37,6 +57,9 @@ class FooController(private val chuckNorrisService: ChuckNorrisService) {
 
     @GetMapping(value = ["/joke"])
     fun chuckNorris() = chuckNorrisService.getJoke()
+
+    @GetMapping(value = ["/foo"])
+    fun foo() = chuckNorrisService.getFoo()
 }
 
 @Service
@@ -52,6 +75,23 @@ class ChuckNorrisService(private val restTemplate: RestTemplate) {
         }
         return chuckNorris
     }
+
+
+    @Retryable(Exception::class, maxAttempts = 5, backoff = Backoff(delay = 1000))
+    fun getFoo() {
+        println("----------- DO SOMETHNING --------------- ")
+        this.restTemplate.getForEntity(URL+"/notavalidurl", ChuckNorris::class.java).body
+    }
+
+    @Recover
+    fun recoverFoo(e: HttpClientErrorException) {
+        // ... panic
+        println("----------- PANIC --------------- ")
+    }
+
+
+
+
 }
 
 data class ChuckNorris(val value: String = "", val url: String = "")
@@ -63,3 +103,13 @@ fun measureTime(block: () -> Unit) {
     val end = System.nanoTime()
     println("Service call took : ${(end - start) / 1.0e9} seconds")
 }
+
+//
+//interface BackendAdapter {
+//    @Retryable(value = [RemoteAccessException::class], maxAttempts = 3, backoff = Backoff(delay = 1000))
+//    fun getBackendResponse(simulateretry: Boolean, simulateretryfallback: Boolean): String
+//
+//    @Recover
+//    fun getBackendResponseFallback(e: RuntimeException): String
+//
+//}
